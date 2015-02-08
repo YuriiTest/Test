a6d9994c56fd6750ce81a93f00d0f4f2ea416412
Yurii <yuriimys@gmail.com>
2015-02-08 20:32:51 +0200

    Some codacy changes

--- tasks/static/tasks/js/upload.js
+++ tasks/static/tasks/js/upload.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
     if (document.getElementById('uploadPreview')){
-        defaultImage =  $('#uploadPreview').attr("src");
+        window.defaultImage =  $('#uploadPreview').attr("src");
     }
 });
 
