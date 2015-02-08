1c4abd1ca5f10b390b9eca554d302411ca8ed192
Yurii <yuriimys@gmail.com>
2015-02-08 23:23:57 +0200

    Some codacy changes

--- tasks/static/tasks/js/upload.js
+++ tasks/static/tasks/js/upload.js
@@ -1,36 +1,35 @@
 $(document).ready(function()
 {
-    if (document.getElementById('uploadPreview')){
-        window.defaultImage =  $('#uploadPreview').attr("src");
+    var uploadPreview = $("#uploadPreview");
+    if (uploadPreview){
+        var defaultImage =  uploadPreview.attr("src");
     }
-});
-
-function PreviewImage()
-{
-    var oFReader = new FileReader();
-    oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
-    oFReader.onload = function (oFREvent) {
-        if (oFREvent.target.result.indexOf("data:image") +1)
+    //123
+    $("#id_select").change(function() {
+        var selectedValue = $("#id_select").val();
+        if (selectedValue === "")
         {
-            $('#uploadPreview').attr("src", oFREvent.target.result);
+            uploadPreview.attr("src", defaultImage);
         }
         else
         {
-            $('#uploadPreview').attr("src", "/static/tasks/img/no-image.png");
+            uploadPreview.attr("src", "/media/" + selectedValue);
         }
-    };
-    $("#display").show();
-}
-
-function PreviewImageSelect()
-{
-    var selectedValue = $("#id_select").val();
-    if (selectedValue === "")
-    {
-        $('#uploadPreview').attr("src", defaultImage);
-    }
-    else
-    {
-        $('#uploadPreview').attr("src", "/media/" + selectedValue);
-    }
-}
+    });
+    //456
+    uploadImage.change(function() {
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+        oFReader.onload = function (oFREvent) {
+            if (oFREvent.target.result.indexOf("data:image") +1)
+            {
+                uploadPreview.attr("src", oFREvent.target.result);
+            }
+            else
+            {
+                uploadPreview.attr("src", "/static/tasks/img/no-image.png");
+            }
+        };
+        $("#display").show();
+    });
+});
